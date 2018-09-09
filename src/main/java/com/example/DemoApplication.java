00199f23c6426a57f04c0500f655b9ede9b60905
ljj <410439770@qq.com>
2018-09-09 23:25:52 +0800

    数据库操作

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.mapper")
+@MapperScan("com.example.mapper.*")
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/controller/DatabaseController.java
+++ src/main/java/com/example/controller/DatabaseController.java
@@ -1,6 +1,6 @@
 package com.example.controller;
 
-import com.mappper.UserMapper;
+import com.example.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
