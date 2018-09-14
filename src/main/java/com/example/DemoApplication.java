2a9d37522921e67011c1a2e44e0adb1f095b32b8
luojunjie <luojunjie@inspur.com>
2018-09-14 16:18:57 +0800

    druid修改

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,15 +1,13 @@
 package com.example;
 
-import com.example.domain.User;
-import com.example.domain.UserRepository;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.example.mapper")
+@ServletComponentScan("com.example.base.servlet")
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
